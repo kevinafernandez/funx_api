15622d382ffeae76baec7666263ed1c6e9d403d2
Kevin Fernandez <kevinafernandezm@gmail.com>
2019-08-31 14:41:40 -0400

    Migrations

---
+++ db/migrate/20190830005549_create_albums.rb
@@ -0,0 +1,13 @@
+class CreateAlbums < ActiveRecord::Migration[6.0]
+  def change
+    create_table :albums do |t|
+      t.string :name
+      t.string :description
+      t.date :release_date
+      t.integer :total_played, default: 0
+      t.references :artist, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
