657aa66422ad7c71d8ff64935cc96d91f3cad5c9
Kevin Fernandez <kevinafernandezm@gmail.com>
2019-09-02 14:43:29 -0400

    Change name method for serializer - part II

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -1,7 +1,7 @@
 class AlbumSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :name, :description, :total_played, :owner
+  attributes :name, :description, :total_played, :artist_name
 
   attribute :release_date do |object|
     object.release_date&.strftime('%d/%m/%Y')
